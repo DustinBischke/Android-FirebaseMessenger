0392df9c9b798fac8e4309e405991f47d41df67e
Dustin Bischke <DustinBischke@gmail.com>
2020-05-24 14:53:26 -0600

    Setup Firebase Authentication

--- app/src/main/java/ca/bischke/firebasemessenger/LoginActivity.kt
+++ app/src/main/java/ca/bischke/firebasemessenger/LoginActivity.kt
@@ -3,20 +3,65 @@ package ca.bischke.firebasemessenger
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private val TAG = "FirebaseMessenger"
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        text_not_registered.setOnClickListener {
+        auth = Firebase.auth
+
+        button_login.setOnClickListener {
             buttonLoginClick()
+        }
+
+        text_not_registered.setOnClickListener {
+            buttonRegisterClick()
         }
     }
 
     private fun buttonLoginClick() {
+        val email = edittext_email.text.toString()
+        val password = edittext_password.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(baseContext, "Email and Password cannot be blank.", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+
+        loginUserAccount(email, password)
+    }
+
+    private fun loginUserAccount(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "Successfully logged in.")
+                    Toast.makeText(baseContext, "Authentication success.", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Failed to log in.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }.addOnFailureListener(this) { e ->
+                Log.w(TAG, "signInWithEmail:failure", e)
+                Toast.makeText(baseContext, "Failed to log in.",
+                    Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun buttonRegisterClick() {
         val intent = Intent(this, RegisterActivity::class.java)
         startActivity(intent)
     }
--- app/src/main/java/ca/bischke/firebasemessenger/RegisterActivity.kt
+++ app/src/main/java/ca/bischke/firebasemessenger/RegisterActivity.kt
@@ -3,20 +3,65 @@ package ca.bischke.firebasemessenger
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+    private val TAG = "FirebaseMessenger"
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        text_already_registered.setOnClickListener {
+        auth = Firebase.auth
+
+        button_register.setOnClickListener {
             buttonRegisterClick()
+        }
+
+        text_already_registered.setOnClickListener {
+            buttonLoginClick()
         }
     }
 
     private fun buttonRegisterClick() {
+        val email = edittext_email.text.toString()
+        val password = edittext_password.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(baseContext, "Email and Password cannot be blank.", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+
+        createUserAccount(email, password)
+    }
+
+    private fun createUserAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "createUserWithEmail:success")
+                    Toast.makeText(baseContext, "Successfully created account.", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Failed to create account.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }.addOnFailureListener(this) { e ->
+                Log.w(TAG, "createUserWithEmail:failure", e)
+                Toast.makeText(baseContext, "Failed to create account.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+    }
+
+    private fun buttonLoginClick() {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
     }
