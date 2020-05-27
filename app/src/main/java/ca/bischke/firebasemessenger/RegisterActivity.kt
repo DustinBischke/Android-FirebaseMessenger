672d15706e75c4a86796f47f1193f2dbfd1c09f7
Dustin Bischke <DustinBischke@gmail.com>
2020-05-27 11:53:29 -0600

    Added Firestore and Storage. Setup username and profile pictures

--- app/src/main/java/ca/bischke/firebasemessenger/RegisterActivity.kt
+++ app/src/main/java/ca/bischke/firebasemessenger/RegisterActivity.kt
@@ -1,24 +1,44 @@
 package ca.bischke.firebasemessenger
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     private val TAG = "FirebaseMessenger"
+    private val REQUEST_IMAGE_CODE = 69
     private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var storage: FirebaseStorage
+    private lateinit var imageUri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         auth = Firebase.auth
+        firestore = Firebase.firestore
+        storage = Firebase.storage
+
+        button_select_photo.setOnClickListener {
+            buttonSelectPhotoClick()
+        }
 
         button_register.setOnClickListener {
             buttonRegisterClick()
@@ -29,26 +49,66 @@ class RegisterActivity : AppCompatActivity() {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            val photoUri = data.data
+            if (photoUri != null) {
+                val bitmap = if (Build.VERSION.SDK_INT < 28) {
+                    MediaStore.Images.Media.getBitmap(this.contentResolver, photoUri)
+                } else {
+                    val source = ImageDecoder.createSource(this.contentResolver, photoUri)
+                    ImageDecoder.decodeBitmap(source)
+                }
+
+                button_select_photo.setImageBitmap(bitmap)
+            }
+        }
+    }
+
+    private fun buttonSelectPhotoClick() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_IMAGE_CODE)
+    }
+
     private fun buttonRegisterClick() {
         val email = edittext_email.text.toString()
         val password = edittext_password.text.toString()
+        val username = edittext_username.text.toString()
 
-        if (email.isEmpty() || password.isEmpty()) {
-            Toast.makeText(baseContext, "Email and Password cannot be blank.", Toast.LENGTH_SHORT)
+        if (email.isEmpty() || password.isEmpty() || username.isEmpty()) {
+            Toast.makeText(baseContext, "All fields cannot be blank.", Toast.LENGTH_SHORT)
                 .show()
             return
         }
 
-        createUserAccount(email, password)
+        createUserAccount(email, password, username)
     }
 
-    private fun createUserAccount(email: String, password: String) {
+    private fun buttonLoginClick() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun createUserAccount(email: String, password: String, username: String) {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     Log.d(TAG, "createUserWithEmail:success")
                     Toast.makeText(baseContext, "Successfully created account.", Toast.LENGTH_SHORT)
                         .show()
+                    val user = auth.currentUser
+
+                    if (user != null) {
+                        val uid = user.uid
+                        createUserFirestore(uid, username)
+
+                        if (::imageUri.isInitialized) {
+                            uploadProfileImage(user, imageUri)
+                        }
+                    }
                 } else {
                     Log.w(TAG, "createUserWithEmail:failure", task.exception)
                     Toast.makeText(baseContext, "Failed to create account.", Toast.LENGTH_SHORT)
@@ -61,8 +121,30 @@ class RegisterActivity : AppCompatActivity() {
             }
     }
 
-    private fun buttonLoginClick() {
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
+    private fun createUserFirestore(uid: String, username: String) {
+        val userHashMap = hashMapOf(
+            "username" to username
+        )
+
+        firestore.collection("users").document(uid)
+            .set(userHashMap)
+            .addOnSuccessListener {
+                Log.d(TAG, "Document successfully written.")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error writing document.", e)
+            }
+    }
+
+    private fun uploadProfileImage(firebaseUser: FirebaseUser, uri: Uri) {
+        val reference = storage.getReference("/images/profile/$firebaseUser.uid")
+
+        reference.putFile(imageUri)
+            .addOnSuccessListener {
+                Log.d(TAG, "Successfully uploaded image.")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Failed to upload image.", e)
+            }
     }
 }
