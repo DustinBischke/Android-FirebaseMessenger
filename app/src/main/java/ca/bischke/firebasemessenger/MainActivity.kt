1826e823e1f2006b234c9001323ca8679b320e9e
Dustin Bischke <DustinBischke@gmail.com>
2020-06-06 13:54:12 -0600

    Added FAB and Menu Icons

--- app/src/main/java/ca/bischke/firebasemessenger/MainActivity.kt
+++ app/src/main/java/ca/bischke/firebasemessenger/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -18,6 +19,10 @@ class MainActivity : AppCompatActivity() {
 
         auth = Firebase.auth
         verifyUserIsLoggedIn()
+
+        fab_message.setOnClickListener {
+            startNewMessageActivity()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
