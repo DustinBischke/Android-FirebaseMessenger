09a49c309d79d25b20d616dc268b93a9a9c68e4b
Dustin Bischke <DustinBischke@gmail.com>
2020-06-11 22:50:31 -0600

    Optimized Message Layouts

--- app/src/main/java/ca/bischke/firebasemessenger/MessageAdapter.kt
+++ app/src/main/java/ca/bischke/firebasemessenger/MessageAdapter.kt
@@ -1,5 +1,6 @@
 package ca.bischke.firebasemessenger
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,6 +15,7 @@ import com.google.firebase.storage.FirebaseStorage
 import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.extensions.LayoutContainer
 import java.text.SimpleDateFormat
+import java.util.*
 
 class MessageAdapter(options: FirestoreRecyclerOptions<Message>) :
     FirestoreRecyclerAdapter<Message, MessageAdapter.ViewHolder>(options) {
@@ -25,59 +27,12 @@ class MessageAdapter(options: FirestoreRecyclerOptions<Message>) :
         } else {
             val view = LayoutInflater.from(parent.context)
                 .inflate(R.layout.layout_message_received, parent, false)
-            ViewHolder(view)
+            ViewHolderReceived(view)
         }
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int, model: Message) {
-        holder.apply {
-            textMessage.text = model.message
-
-            val date = model.timestamp.toDate()
-            val dateFormat = SimpleDateFormat("MMMM dd, yyyy")
-            val timeFormat = SimpleDateFormat("h:mm a")
-
-            textTime.text = timeFormat.format(date).toString()
-
-            val uid = model.uid
-            val reference = FirebaseStorage.getInstance().reference.child("images/profile/$uid")
-
-            reference.downloadUrl.addOnSuccessListener {
-                Glide.with(imageProfile)
-                    .load(reference)
-                    .centerCrop()
-                    .into(imageProfile)
-            }.addOnFailureListener {
-
-            }
-
-            if (position == 0) {
-                textDate.text = dateFormat.format(date).toString()
-            }
-
-            // Date
-            if (position > 0) {
-                val previousDate = getItem(position - 1).timestamp.toDate()
-
-                if (dateFormat.format(date) == dateFormat.format(previousDate)) {
-                    textDate.visibility = View.GONE
-                } else {
-                    textDate.text = dateFormat.format(date).toString()
-                }
-            }
-
-            // Time
-            if (position < itemCount - 1) {
-                if (model.uid == getItem(position + 1).uid) {
-                    val nextDate = getItem(position + 1).timestamp.toDate()
-
-                    if (timeFormat.format(date) == timeFormat.format(nextDate)) {
-                        textTime.visibility = View.GONE
-                        imageProfile.visibility = View.INVISIBLE
-                    }
-                }
-            }
-        }
+        holder.bind(model, position)
     }
 
     override fun getItemViewType(position: Int): Int {
@@ -88,10 +43,79 @@ class MessageAdapter(options: FirestoreRecyclerOptions<Message>) :
         return position.toLong()
     }
 
-    inner class ViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
-        val textMessage: TextView = itemView.findViewById(R.id.text_message)
-        val textTime: TextView = itemView.findViewById(R.id.text_received)
-        val textDate: TextView = itemView.findViewById(R.id.text_date)
-        val imageProfile: CircleImageView = itemView.findViewById(R.id.imageview_profile)
+    open inner class ViewHolder(override val containerView: View) :
+        RecyclerView.ViewHolder(containerView), LayoutContainer {
+        private val textMessage: TextView = itemView.findViewById(R.id.text_message)
+        private val textTime: TextView = itemView.findViewById(R.id.text_received)
+        private val textDate: TextView = itemView.findViewById(R.id.text_date)
+
+        @SuppressLint("SimpleDateFormat")
+        private val dateFormat = SimpleDateFormat("MMMM dd, yyyy")
+        @SuppressLint("SimpleDateFormat")
+        protected val timeFormat = SimpleDateFormat("h:mm a")
+
+        protected lateinit var date: Date
+
+        open fun bind(model: Message, position: Int) {
+            textMessage.text = model.message
+
+            date = model.timestamp.toDate()
+
+            if (position != 0) {
+                val previousDate = getItem(position - 1).timestamp.toDate()
+
+                if (dateFormat.format(date) == dateFormat.format(previousDate)) {
+                    textDate.visibility = View.GONE
+                }
+            }
+
+            if (textDate.visibility == View.VISIBLE) {
+                textDate.text = dateFormat.format(date).toString()
+            }
+
+            if (position < itemCount - 1) {
+                if (model.uid == getItem(position + 1).uid) {
+                    val nextDate = getItem(position + 1).timestamp.toDate()
+
+                    if (timeFormat.format(date) == timeFormat.format(nextDate)) {
+                        textTime.visibility = View.GONE
+                    }
+                }
+            }
+
+            if (textTime.visibility == View.VISIBLE) {
+                textTime.text = timeFormat.format(date).toString()
+            }
+        }
+    }
+
+    inner class ViewHolderReceived(containerView: View) : ViewHolder(containerView) {
+        private val imageProfile: CircleImageView = itemView.findViewById(R.id.imageview_profile)
+
+        override fun bind(model: Message, position: Int) {
+            super.bind(model, position)
+
+            if (position < itemCount - 1) {
+                if (model.uid == getItem(position + 1).uid) {
+                    val nextDate = getItem(position + 1).timestamp.toDate()
+
+                    if (timeFormat.format(date) == timeFormat.format(nextDate)) {
+                        imageProfile.visibility = View.INVISIBLE
+                    }
+                }
+            }
+
+            if (imageProfile.visibility == View.VISIBLE) {
+                val uid = model.uid
+                val reference = FirebaseStorage.getInstance().reference.child("images/profile/$uid")
+
+                reference.downloadUrl.addOnSuccessListener {
+                    Glide.with(imageProfile)
+                        .load(reference)
+                        .centerCrop()
+                        .into(imageProfile)
+                }
+            }
+        }
     }
 }
